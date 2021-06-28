6e4bbba4bdfc87151370a90e9a869e1929203c99
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-28 18:22:37 -0400

    Create SecretContainer file

---
+++ src/containers/SecretContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SecretContainer extends Component {
+  componentDidMount() {
+
+  }
+  
+  render() {
+    return null
+  }
+}
+
+export default connect()(SecretContainer);
