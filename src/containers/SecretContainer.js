75b1b90d838677f5ecd1b80c6715fc2c7000022f
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-28 20:31:24 -0400

    Add fetch to container

--- src/containers/SecretContainer.js
+++ src/containers/SecretContainer.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchSecrets } from '../actions/secretActions';
 
 class SecretContainer extends Component {
   componentDidMount() {
-
+    this.props.dispatch(fetchSecrets());
   }
   
   render() {
