3e84916f8748eaee5228ee05e30766547e14bee9
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-29 16:27:04 -0400

    Add logic to FormContainer

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import { Component } from 'react';
+import Form from '../components/Form';
 
-export default class FormContainer extends Component {
+class FormContainer extends Component {
+  state = {
+    text: "",
+    tag: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div>
-        
+        <Form 
+          formData={this.state}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
       </div>
     )
   }
 }
+
+export default FormContainer;
