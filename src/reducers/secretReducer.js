b7d224422b6383744feb8dfb9c38a269b8091f40
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-28 22:35:55 -0400

    Fix fetches in reducer

--- src/reducers/secretReducer.js
+++ src/reducers/secretReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_SECRETS_BEGIN, FETCH_SECRETS_SUCCESS, FETCH_SECRETS_FAILURE } from '../constants/ActionTypes';
 
 const initialState = {
-  byId: new Map(),
+  list: [],
   allIds: []
 }
 
@@ -13,17 +13,11 @@ const secretReducer = function reducer(
     case FETCH_SECRETS_BEGIN:
       return state;
     case FETCH_SECRETS_SUCCESS:
-      debugger
-      // ! MANIPULATE DATA HERE
-      let sIds = action.payload.data.map(secret => parseInt(secret.id, 10))
-      let sData = action.payload.data.map(secret => secret.attributes)
-      let map = new Map();
-      for (let i = 0; i < sIds.length; i++) {
-        map.set(sIds[i], sData[i]);
-      }
+      let sIds = action.payload.data.map(secret => parseInt(secret.id, 10));
+      let sData = action.payload.data.map(secret => secret.attributes);
       return {
         ...state,
-        byId: map,
+        list: sData,
         allIds: sIds
       };
     case FETCH_SECRETS_FAILURE:
