6a460187d2e4432a47afd66f71c6e5b996201073
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-29 19:59:06 -0400

    Add secret tag and styling

--- src/components/SecretListItem.js
+++ src/components/SecretListItem.js
@@ -1,3 +1,7 @@
-const SecretListItem = props => <p>{props.secret.content}</p>
+const spanStyle = {
+  paddingLeft: '30px',
+  fontWeight: 'bold'
+}
+const SecretListItem = props => <p>{props.secret.content} <span style={spanStyle}>{props.secret.tag}</span></p>
 
 export default SecretListItem;
