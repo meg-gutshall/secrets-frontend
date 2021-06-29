c44e4f809b8681cd0b95fd28446422094c3b8371
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-29 16:09:25 -0400

    Create FormContainer file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import SecretContainer from './containers/SecretContainer';
+import FormContainer from './containers/FormContainer';
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
+        <FormContainer />
         <SecretContainer />
       </div>
     );
