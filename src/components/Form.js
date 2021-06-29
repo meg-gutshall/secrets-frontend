18a7cca7ad8a48cb92f5944fa09966b07e6faf9e
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-29 17:33:59 -0400

    Finish form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,24 @@ const Form = props => {
     <div>
       <h2>What's Your Secret?</h2>
       <form onSubmit={props.handleSubmit}>
-        <input 
-          type="textarea"
+        <label htmlFor="secretContent">Confess your secret here:</label><br/>
+        <textarea
+          id="secretContent"
           name="content"
           onChange={props.handleChange}
           value={props.formData.text}
-        />
-        <input 
-          type="text"
+        ></textarea><br/>
+        <label htmlFor="secretTag">Choose a tag:</label><br/>
+        <select
           name="tag"
-          onChange={props.handleChange}
           value={props.formData.tag}
-        />
+          onChange={props.handleChange}
+        >
+          <option value="juicy">juicy</option>
+          <option value="silly">silly</option>
+          <option value="dirty">dirty</option>
+          <option value="deep">deep</option>
+        </select><br/>
         <button type="submit">Submit</button>
       </form>
     </div>
