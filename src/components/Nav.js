ac58205924958ab73e6db49af2f070d662301f08
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-30 03:10:03 -0400

    Create a nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,11 @@
 import { Link } from 'react-router-dom';
 
 const Nav = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/secrets">Secrets</Link>
-    </li>
-    <li>
-      <Link to="/secrets/new">New Secret</Link>
-    </li>
-  </ul>
+  <div>
+    <Link to="/secrets">Secrets</Link><br/>
+    <Link to="/secrets/new">Spill!</Link><br/>
+    <Link to="/secrets/about">About</Link><br/>
+  </div>
 );
 
 export default Nav;
