0ffeb94fe4519613653809788a99ba9de03944a4
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-30 03:09:18 -0400

    Turn 'Home' into 'About'

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <>
+    <h1>About</h1>
+    <p>Here's a bit about this app!</p>
+  </>
+);
+
+export default About;
--- src/components/Home.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Home = () => <h1>Secrets</h1>
-
-export default Home;
