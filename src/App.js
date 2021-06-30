20079c39cd2be5853b89fd7bdcbe4fd43712b48e
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-30 03:08:49 -0400

    Move routing to its own file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SecretContainer from './containers/SecretContainer';
-import FormContainer from './containers/FormContainer';
-import Home from './components/Home';
-import Nav from './components/Nav';
+import Routes from './components/Routes';
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Nav />
-          {/* <Switch> */}
-          <Route exact path='/' component={Home} />
-          <Route path='/secrets/new' component={FormContainer} />
-          <Route path='/secrets' component={SecretContainer} />
-          {/* </Switch> */}
-        </div>
-      </Router>
+      <div className="App">
+        <Routes />
+      </div>
     );
   }
 }
