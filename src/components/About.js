a7de3f23258bfe30b8b3d0d7ea49b339b9baa19f
Meg Gutshall <meghan.gutshall@gmail.com>
2021-07-13 16:00:41 -0400

    Change verbiage

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 const About = () => (
   <>
     <h1>About</h1>
-    <p>Here's a bit about this app!</p>
+    <p>Everyone has their own secrets—what are yours?</p>
+    <h2>How To Use</h2>
+    <p>Simply visit the "Share Your Secret" link and type your secret into the textbox underneath "Spill your secret here." Then choose a tag from the "Choose a tag" drop-down menu and click the "Confess" button to see your secret appear in the list!</p>
   </>
 );
 
