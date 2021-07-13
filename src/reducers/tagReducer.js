104e811b8f33dfef359472cd4305f80e0dbb26ef
Meg Gutshall <meghan.gutshall@gmail.com>
2021-07-13 00:28:45 -0400

    Add post reducer actions

--- src/reducers/tagReducer.js
+++ src/reducers/tagReducer.js
@@ -1,4 +1,4 @@
-import { BEGIN_FETCH, GET_TAGS_SUCCESS, GET_TAGS_FAILURE } from '../constants/ActionTypes';
+import { BEGIN_FETCH, GET_TAGS_SUCCESS, GET_TAGS_FAILURE, POST_TAG_SUCCESS, POST_TAG_FAILURE } from '../constants/ActionTypes';
 
 const initialState = {
   byId: {},
@@ -17,14 +17,25 @@ const tagReducer = function reducer(
         isLoading: true
       };
     case GET_TAGS_SUCCESS:
-      let tagsArray = action.payload.data;
-      let tIds = tagsArray.map(tag => parseInt(tag.id, 10));
+      let tIds = action.payload.included.map(tag => parseInt(tag.id, 10));
       return {
-        byId: tagsArray.reduce((acc, cv) => ({...acc, [cv.id]: cv.attributes}), {}),
+        byId: action.payload.included.reduce((acc, cv) => ({...acc, [cv.id]: cv.attributes}), {}),
         allIds: tIds,
         isLoading: false
       };
     case GET_TAGS_FAILURE:
+      return action.payload;
+    case POST_TAG_SUCCESS:
+      let tId = parseInt(action.payload.included.id, 10);
+      return {
+        byId: {
+          ...state.byId,
+          tId: action.payload.included.attributes
+        },
+        allIds: state.allIds.concat(tId),
+        isLoading: false
+      };
+    case POST_TAG_FAILURE:
       return action.payload;
     default:
       return state;
