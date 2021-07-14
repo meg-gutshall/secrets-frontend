1ba8aa8e7fe7fae79d634845418e8f7b8002be95
Meg Gutshall <meghan.gutshall@gmail.com>
2021-07-14 16:18:59 -0400

    [WIP]: Checkbox logic

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -2,29 +2,61 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import { postSecret } from '../actions/allActions';
 import Form from '../components/Form';
+import FormCheckbox from '../components/FormCheckbox';
 
 class FormContainer extends Component {
-  state = {
-    content: '',
-    tag: 'default'
+  availableTags = () => this.props.tags;
+
+  createCheckbox = tag => {
+    <FormCheckbox
+      id={tag.id}
+      name={tag.name}
+      checked={this.state.tags[tag]}
+      onChange={this.handleCheck}
+    />
   }
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  createCheckboxes = () => this.availableTags().map(this.createCheckbox);
+
+  handleCheck = e => {
+    const tagId = e.target.id;
+    this.setState(previousState => ({
+      tags: { ...previousState.tags, [tagId]: !previousState.tags[tagId] }
+    }))
+  }
+
+  state = {
+    content: '',
+    tags: this.availableTags.reduce(
+      (tags, tag) => ({ ...tags, [tag.id]: false }), {}
+    )
+  }
+
+  handleContent = e => this.setState({ content: e.target.value });
 
   handleSubmit = e => {
     e.preventDefault();
     console.log(this.state);
-    this.props.postSecret(this.state);
-    this.setState({ content: '', tag: 'default' });
+    const tags = this.state.tags.filter(tagId => this.state.tags[tagId])
+    const newSecret = {
+      content: this.state.content,
+      tags: tags
+    }
+    this.props.postSecret(newSecret);
+    this.setState({ content: '', tags: [] });
     // A nice todo --> Only clear the form if the POST is successful (handleSuccess callback method to pass to the reducer)
   }
-
+  
   render() {
+    console.log(this.props)
     return (
       <div>
         <Form
-          formData={this.state}
-          handleChange={this.handleChange}
+          content={this.state.content}
+          handleContent={this.handleContent}
+          tags={this.props.tags}
+          isChecked={this.isChecked}
+          handleCheck={this.handleCheck}
           handleSubmit={this.handleSubmit}
         />
       </div>
@@ -32,4 +64,10 @@ class FormContainer extends Component {
   }
 }
 
-export default connect(null, { postSecret })(FormContainer);
+const mapStateToProps = state => {
+  return {
+    tags: state.tags.byId
+  }
+}
+
+export default connect(mapStateToProps, { postSecret })(FormContainer);
